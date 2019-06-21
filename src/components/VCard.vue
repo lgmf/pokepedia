9be406fd32c2740f16db89f317196df16d336e21
Luiz Guilherme <lgm.ferrari@gmail.com>
2019-06-21 19:22:34 -0300

    feat(type): create the card component (#2)

---
+++ src/components/VCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="v-card">
+    <h2
+      class="title"
+      :class="{ '-success': color === 'success', '-danger': color === 'danger'}"
+    >{{ title }}</h2>
+    <div class="body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'v-card',
+})
+export default class VCard extends Vue {
+  @Prop()
+  title!: string;
+
+  @Prop()
+  color!: 'success'|'danger'|'default';
+}
+</script>
+
+<style scoped lang="scss">
+.v-card {
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-gap: 40px;
+  background: #fff;
+  border-radius: 2px;
+  padding: 36px;
+  position: relative;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+
+  & > .title {
+    font-size: 24px;
+    font-weight: 600;
+
+    &.-success {
+      color: darkseagreen;
+    }
+
+    &.-danger {
+      color: indianred;
+    }
+  }
+}
+</style>
