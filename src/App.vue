58148883c14a724155ef943b84cd9641e57c80e8
Luiz Guilherme <lgm.ferrari@gmail.com>
2019-06-21 17:24:15 -0300

    feat(types): fetch type data from pokeapi (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="nav">
+      <router-link to="/types">Types</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import url("assets/styles/main.scss");
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Montserrat", sans-serif;
 }
-#nav {
+.nav {
   padding: 30px;
   a {
+    font-size: 40px;
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
+
     &.router-link-exact-active {
       color: #42b983;
     }
---
+++ src/components/OutlineButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button class="outline-button" :class="{ '-active': active }" @click="onClick()">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'outline-button',
+})
+export default class OutlineButton extends Vue {
+  private active: boolean = false;
+
+  onClick() {
+    this.active = !this.active;
+    this.$emit('clicked');
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.outline-button {
+  display: flex;
+  justify-content: center;
+  background: white;
+  color: darkslateblue;
+  padding: 20px 32px;
+  cursor: pointer;
+  border: 2px solid cornflowerblue;
+  border-radius: 8px;
+  transition: background 0.35s ease-in-out;
+  outline: none;
+  text-transform: uppercase;
+  font-size: 18px;
+
+  &:focus {
+    border-color: darkslateblue;
+  }
+
+  &.-active {
+    background: darkslateblue;
+    color: white;
+  }
+}
+</style>
