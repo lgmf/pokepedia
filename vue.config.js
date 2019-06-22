5028289c15b74a13ef7c8f70b0a5fb04b265d5de
Luiz Guilherme <lgm.ferrari@gmail.com>
2019-06-22 18:51:53 -0300

    fix(global styles): importa global styles on vueconfig (#4)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/pokepedia/'
-    : '/'
+    : '/',
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "@/styles/font.scss";
+          @import "@/styles/pokemon-types-colors.scss";
+          @import "@/styles/reset.scss";
+        `
+      }
+    }
+  }
 }
